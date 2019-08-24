42efa7b7aad00cf14d03b344b02ea6146402a653
mukjos30 <mukjos30@gmail.com>
2019-08-24 14:33:38 +0200

    add form

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import{FormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteComponent } from './quote/quote.component';
+
+import { DateCountPipe } from './date-count.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+    QuoteDetailsComponent,
+    QuoteFormComponent,
+    QuoteComponent,
+   
+    DateCountPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
